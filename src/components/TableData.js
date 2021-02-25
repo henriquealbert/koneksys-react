945d9214adb0b7455c19e9e572c8a5eeddfa79c4
Henrique <ishenriquealbert@gmail.com>
2021-02-25 14:57:41 -0300

    added tabledata component

---
+++ src/components/TableData.js
@@ -0,0 +1,34 @@
+export function TableData() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Player</th>
+          <th>#</th>
+          <th>Pos</th>
+          <th>Height</th>
+          <th>Weight</th>
+          <th>Experience</th>
+          <th>Age</th>
+          <th>Salary</th>
+          <th>College</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Player Name</td>
+          <td>00</td>
+          <td>XX</td>
+          <td>X-X</td>
+          <td>XXX</td>
+          <td>X</td>
+          <td>X</td>
+          <td>XM</td>
+          <td>College Name</td>
+          <td>Status</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
